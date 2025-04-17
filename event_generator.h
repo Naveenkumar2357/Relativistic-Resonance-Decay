a194ffa64872236943ca3a17f27363f6f8241389
Naveenkumar Senthilkumar <naveenkumar75388@gmail.com>
2025-04-17 01:28:29 -0400

    initial commit

---
+++ event_generator.h
@@ -0,0 +1,2 @@
+// this a class of function which perform the decay and calculate invariant mass... stores everything in a obeject called track_1
+//
---
+++ track.h
@@ -0,0 +1,10 @@
+// this is a header file of class names track
+//
+// this track class can hold every information corresponds to a single event
+//
+//constant of the events like masss of L, p , pi
+//Pt= if fixed
+// 4 momentum of lambda, proton, pions
+// randomly sampled eta , phi
+// randomly sampled phi 1, theta 1 (decay in rest frame)
+// invariant mass
