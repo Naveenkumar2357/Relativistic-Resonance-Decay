fba220bdbd459961aee4369d6d2ff9fe87d3ef2b
Naveenkumar Senthilkumar <naveenkumar75388@gmail.com>
2025-04-23 16:42:33 -0400

    event generator completed

---
+++ event_generator.cpp
@@ -0,0 +1,111 @@
+#include "eventgenerator.h"
+#include "inputfile.h"
+#include <cmath>
+
+
+//constructor and destructor
+eventgenerator::eventgenerator() {
+    mineta = etamin;
+    maxeta = etamax;
+    random_sampling.SetSeed(time(0));
+}
+
+
+eventgenerator::~eventgenerator() {}
+
+
+//memberfunction
+//random sampling // eta, pt, and phi for Lambda
+void eventgenerator::generate_event() {
+
+    double m_lambda = lambdamass;  // Lambda, proton, pion mass read from headerinputfile and stored in dummy variable 
+    double m_proton = protonmass;  
+    double m_pion = pionmass;      
+
+
+    
+    double eta_L = random_sampling.Uniform(mineta, maxeta);
+    double pt_L = random_sampling.Uniform(ptmin, ptmax);
+    double phi_L = random_sampling.Uniform(0, 2 * M_PI);
+
+    double pz_L = pt_L * sinh(eta_L);    // eta is psueedo rapidity
+    double px_L = pt_L * cos(phi_L);
+    double py_L = pt_L * sin(phi_L);
+
+    // object track created and all information is being stored
+    track.setP4_L(px_L, py_L, pz_L, m_lambda);
+    track.seteta_L(eta_L);
+    track.setphi_L(phi_L);
+    track.setpt_L(pt_L);
+
+
+      // formula to find the 3 momentum of decaying particles in lambda's rest frame(eta_L , phi_L)
+      // P_p = - P_pi = p3_magnitude 
+    double p3_magnitude = sqrt( (m_lambda * m_lambda - (m_proton + m_pion) * (m_proton + m_pion)) * (m_lambda * m_lambda - (m_proton - m_pion) * (m_proton - m_pion)) ) / (2 * m_lambda); // magnitude of 3 momentum of proton and pion
+
+    //isotropically performing decay
+    //sampling theta and phi for decaying particles
+    double phi_p = random_sampling.Uniform(0, 2 * M_PI);  // Azimuthal 
+    double theta_p = random_sampling.Uniform(0, M_PI);  // Polar 
+
+    //using spherical co ordinates to find the momentum components
+    double px_p_rest = p_magnitude * sin(theta_p) * cos(phi_p);
+    double py_p_rest = p_magnitude * sin(theta_p) * sin(phi_p);
+    double pz_p_rest = p_magnitude * cos(theta_p);
+
+    // Step 7: Momentum of the pion is opposite to that of the proton
+    double px_pi_rest = -px_p_rest;
+    double py_pi_rest = -py_p_rest;
+    double pz_pi_rest = -pz_p_rest;
+
+    //storing this momentum in rest frame for a moment
+    track.setP4_p(px_p_rest, py_p_rest, pz_p_rest, m_proton);
+    track.setP4_pi(px_pi_rest, py_pi_rest, pz_pi_rest, m_pion);
+
+    //boosting back to lab frame(eta_L, phi_L)
+    track.P4_p.Boost(track.P4_L.Vect());  
+    track.P4_pi.Boost(track.P4_L.Vect());
+
+    LorentzVector<PxPyPzM4D<double>> P4_total = P4_p + P4_pi;
+    double invariant_mass = P4_total.M();
+   
+
+    //invariant mass
+    track.setinvariantmass(invariant_mass);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+Track eventgenerator::getTrack() const {
+    return track;
+}
+
--- track.h
+++ track.h
@@ -16,6 +16,7 @@ public:
     LorentzVector<PxPyPzM4D<double>> P4_pi;
 
     double eta_L;
+    double phi_L;
     double phi_p;
     double theta_p;
     double invariantmass;
@@ -26,6 +27,8 @@ public:
         P4_pi.SetPxPyPzM(0, 0, 0, pionmass);
 
         eta_L = 0;
+	pt_L= 0;
+	phi_L=0;
         phi_p = 0;
         theta_p = 0;
         invariantmass = 0;
@@ -45,18 +48,27 @@ public:
         P4_pi.SetPxPyPzM(px_pi, py_pi, pz_pi, mass_pi);
     }
 
+
+
+
     void seteta_L(double eta_L_value) {
         eta_L = eta_L_value;
     }
+    void setphi_L(double phi_L_value) {
+        phi_L = phi_L_value;
+    }
+    void setpt_L(double pt_L_value) {
+        pt_L = pt_L_value;
+    }
+
+
 
     void setphi_p(double phi_p_value) {
         phi_p = phi_p_value;
     }
-
-    void settheta_pi(double theta_pi_value) {
-        theta_pi = theta_pi_value;
+    void settheta_p(double theta_pi_value) {
+        theta_p = theta_pi_value;
     }
-
     void setinvariantmass(double mass) {
         invariantmass = mass;
     }
@@ -71,10 +83,11 @@ public:
     double getinvariantmass() const { return invariantmass; }
 
     void printInfo() const {
-        cout << "Lambda 4-momentum: (" << P4_L.Px() << ", " << P4_L.Py() << ", " << P4_L.Pz() << ", " << P4_L.E() << ")\n";
-        cout << "Proton 4-momentum: (" << P4_p.Px() << ", " << P4_p.Py() << ", " << P4_p.Pz() << ", " << P4_p.E() << ")\n";
-        cout << "Pion 4-momentum: (" << P4_pi.Px() << ", " << P4_pi.Py() << ", " << P4_pi.Pz() << ", " << P4_pi.E() << ")\n";
+        cout << "Lambda 4-momentum: (" << P4_L.Px() << ", " << P4_L.Py() << ", " << P4_L.Pz() << ", " << P4_L.M() << ")\n";
+        cout << "Proton 4-momentum: (" << P4_p.Px() << ", " << P4_p.Py() << ", " << P4_p.Pz() << ", " << P4_p.M() << ")\n";
+        cout << "Pion 4-momentum: (" << P4_pi.Px() << ", " << P4_pi.Py() << ", " << P4_pi.Pz() << ", " << P4_pi.M() << ")\n";
         cout << "Lambda eta: " << eta_L << "\n";
+	cout << "Lambda phi: " << phi_L << "\n";
         cout << "Proton/pion phi: " << phi_p << "\n";
         cout << "Proton/pion theta: " << theta_pi << "\n";
     }
